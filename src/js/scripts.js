d5e06a2c82bc73035a86484baa38a309e0f2d548
Alexander Vazhenin <insaneall1@gmail.com>
2019-08-13 17:02:17 +0100

    Added timestamp and comparison functions

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,14 @@
+//current date in Unicode
+var currentDate = Math.floor(Date.now() / 1000);
+
 let dataAboutOrg = [];
 $(document).ready(function() { 
     $.getJSON('https://frontend.camp.dev.unit6.ru/get-slides', function(data) {
         dataAboutOrg = data;
         let filteredOrgData = dataAboutOrg
             .filter(companyData => (companyData.active === true 
-            // Уточнить насчет значений currentdate и enddate
-            // && company.start <= currentdate &&
-            //    company.end >= currentdate
+              && companyData.startDate <= currentDate 
+              && companyData.endDate >= currentDate
             ))
             .sort((a, b) => (a.order > b.order ? 1 : -1))
             ;
